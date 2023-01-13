d5fc0b97937dc3d4cde37d86ced3e703d9d585f5
Jasmin Makasana <jasminmakasana1991@gmail.com>
2023-01-13 16:05:15 +0530

    cors error fixed.

--- server/server.go
+++ server/server.go
@@ -135,7 +135,8 @@ func (s *Server) Stop() {
 
 func corsAllowed(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3002")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 		next.ServeHTTP(w, r)
 	})
